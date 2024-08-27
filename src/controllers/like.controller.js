2d7cc465f47a450b76335b689aac3c10e9f5d446
Krish Chothani <143370415+KrishChothani@users.noreply.github.com>
2024-08-27 09:18:52 +0530

    like and healcheck final

--- src/controllers/like.controller.js
+++ src/controllers/like.controller.js
@@ -13,9 +13,11 @@ const toggleVideoLike = asyncHandler(async (req, res) => {
         throw new ApiError(404 , "Missing videoId")
     }
 
-    const existingVideoLike = await Video.findOne({
+    const existingVideoLike = await Like.findOne({
+        video : videoId,
         LikedBy : req.user._id
     })
+    console.log(existingVideoLike)
     if(existingVideoLike){
         await existingVideoLike.deleteOne()
 
@@ -25,31 +27,107 @@ const toggleVideoLike = asyncHandler(async (req, res) => {
     }
     else{
         await Like.create({
+            video : videoId,
             LikedBy : req.user._id
         })
-
         return res.status(200).json(
             new Apiresponse(201,1,"Add Like Successful")
         )
     }
-
-
 })
 
 const toggleCommentLike = asyncHandler(async (req, res) => {
     const {commentId} = req.params
     //TODO: toggle like on comment
+    if(!commentId){
+        throw new ApiError(401, "Missing CommentId")
+    }
+
+    const existingCommentLike = await Like.findOne({
+        comment : commentId,
+        LikedBy : req.user._id
+    })
+
+    if(existingCommentLike){
+        await existingCommentLike.deleteOne();
+
+        return res.status(200).json(
+            new Apiresponse(200, 0, "Remove Like from comment")
+        )
+    }
+    else {
+        await Like.create({
+            comment : commentId,
+            LikedBy: req.user._id
+        })
+        return res.status(200).json(
+            new Apiresponse(200, 1, "Remove Like from comment")
+        )
+    }
 
 })
 
 const toggleTweetLike = asyncHandler(async (req, res) => {
     const {tweetId} = req.params
-    //TODO: toggle like on tweet
+    if(!tweetId){
+        throw new ApiError(401, "Missing TweetId")
+    }
+
+    const existingTweetLike = await Like.findOne({
+        tweet : tweetId,
+        LikedBy : req.user._id
+    })
+
+    if(existingTweetLike){
+        await existingTweetLike.deleteOne();
+
+        return res.status(200).json(
+            new Apiresponse(200, 0, "Remove Like from Tweet")
+        )
+    }
+    else {
+        await Like.create({
+            tweet : tweetId,
+            LikedBy: req.user._id
+        })
+        return res.status(200).json(
+            new Apiresponse(200, 1, "Remove Like from Tweet")
+        )
+    }
+
 }
 )
 
 const getLikedVideos = asyncHandler(async (req, res) => {
     //TODO: get all liked videos
+    const likedvideo = await Like.aggregate([
+        {
+            $match:{
+                LikedBy : req.user._id
+            }
+        },
+        {
+
+            $lookup:{
+                from : "videos",
+                localField: "video",
+                foreignField: "_id",
+                as: "videoDetails"
+            }
+        },
+        {
+            $unwind : "$videoDetails"
+        }
+    ])
+    console.log(likedvideo)
+
+    if(!likedvideo){
+        throw new ApiError(401 , "something went wrong when trying to collect liked videos")
+    }
+
+    return res.status(200).json(
+        new Apiresponse(200,likedvideo, "liked video fetch successfully")
+    )
 })
 
 export {
